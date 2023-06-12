1dcbde51bb7a10b011a0262fec0352098c7d5f3c
Sobunge <samuelobunge@gmail.com>
2023-06-12 21:46:43 +0300

    first commit

---
+++ src/main/java/com/pensasha/emoney/EmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.pensasha.emoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pensasha/emoney/EmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pensasha.emoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
