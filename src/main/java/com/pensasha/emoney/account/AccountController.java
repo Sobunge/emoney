a72ba7744d612e278855d03d9a10e5b595af8abd
Samuel Obunge <samuelobunge@gmail.com>
2023-06-29 09:40:47 +0300

    addind account controller methods

--- src/main/java/com/pensasha/emoney/account/AccountController.java
+++ src/main/java/com/pensasha/emoney/account/AccountController.java
@@ -1,15 +1,18 @@
 package com.pensasha.emoney.account;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+
 
 @Controller
 public class AccountController {
@@ -55,8 +58,27 @@ public class AccountController {
     }
 
     // Deleting account
-    
+    @GetMapping("/accounts/{id}")
+    public RedirectView deleteAccount(@PathVariable Long id, RedirectAttributes redit, Principal principal) {
+
+        if (accountService.doesAccountExistById(id)) {
+            accountService.deleteAccount(id);
+            redit.addFlashAttribute("success", "Account deleted successfully.");
+        } else {
+            redit.addFlashAttribute("fail", "Account does not exist.");
+        }
+
+        return new RedirectView("/accounts", true);
+    }
 
     // Getting all accounts
+    @GetMapping("/accounts")
+    public String gettingAllAccounts(Model model, Principal principal){
+
+        List<Account> accounts = accountService.getAllAccounts();
+        model.addAttribute("accounts", accounts);
+
+        return "accountsPages/accounts";
+    }
 
 }
