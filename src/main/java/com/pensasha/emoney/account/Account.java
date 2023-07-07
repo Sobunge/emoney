48eaa683cb65f41f0bb9760f4fd97c6fd2c40246
Samuel Obunge <samuelobunge@gmail.com>
2023-07-07 21:32:32 +0300

    addding validation protocal to account class

--- src/main/java/com/pensasha/emoney/account/Account.java
+++ src/main/java/com/pensasha/emoney/account/Account.java
@@ -7,6 +7,7 @@ import com.pensasha.emoney.transaction.Transaction;
 import com.pensasha.emoney.user.User;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderColumn;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -36,10 +38,18 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "account_seq")
     private Long id;
 
-    @NotNull(message = "Name should be entered.")
+    @NotNull
     @Size(min = 2, max = 32, message = "Name should be between 2 and 32 characters.")
+    @Column(length = 32)
     private String name;
+
+    @NotNull
+    @Size(min = 5, max = 254, message = "Account description characters should be between 5 and 254 characters.")
+    @Column(length = 254)
     private String description;
+
+    @NotNull
+    @Min(0)
     private int balance;
 
     @JsonIgnore
@@ -51,5 +61,11 @@ public class Account {
     @JsonIgnore
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     private List<Transaction> transactions;
+
+    public Account(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.balance = 0;
+    }
 
 }
