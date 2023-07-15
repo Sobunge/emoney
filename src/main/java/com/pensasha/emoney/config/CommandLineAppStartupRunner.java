705ea32c36b16e0f57ca1566b6b9c6d7d843c503
Sobunge <samuelobunge@gmail.com>
2023-07-15 13:52:28 +0300

    adding chama account when application is loaded

--- src/main/java/com/pensasha/emoney/config/CommandLineAppStartupRunner.java
+++ src/main/java/com/pensasha/emoney/config/CommandLineAppStartupRunner.java
@@ -1,27 +1,38 @@
 package com.pensasha.emoney.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.pensasha.emoney.account.Account;
+import com.pensasha.emoney.account.AccountService;
 import com.pensasha.emoney.enums.Role;
 import com.pensasha.emoney.user.User;
 import com.pensasha.emoney.user.UserService;
 
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
-    
-    @Autowired
+
     private UserService userService;
+    private AccountService accountService;
+
+    public CommandLineAppStartupRunner(UserService userService, AccountService accountService) {
+        this.userService = userService;
+        this.accountService = accountService;
+    }
 
     public void run(String... args) throws Exception {
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    
-        User user = new User(32906735,"Samuel", "Odhiambo", "Obunge", 0707335375, encoder.encode("samuel1995"), Role.ADMIN);
 
+        User user = new User(32906735, "Samuel", "Odhiambo", "Obunge", 0707335375, encoder.encode("samuel1995"),
+                Role.ADMIN);
         userService.addUser(user);
+
+        if (!accountService.doesAccountNameExist("Chama")) {
+            Account account = new Account("Chama", "Chama account");
+            accountService.addAccount(account);
+        }
 
     }
 }
