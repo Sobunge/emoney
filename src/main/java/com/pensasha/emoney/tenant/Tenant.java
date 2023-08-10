b9d2cfd005b26cd795fe29c87361689d00a7f6bc
Samuel Odhiambo <samuelobunge@gmail.com>
2023-08-11 01:07:07 +0300

    cleaning tenant class

--- src/main/java/com/pensasha/emoney/tenant/Tenant.java
+++ src/main/java/com/pensasha/emoney/tenant/Tenant.java
@@ -3,16 +3,12 @@ package com.pensasha.emoney.tenant;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.pensasha.emoney.enums.Residency;
 import com.pensasha.emoney.houseStatus.HouseStatus;
 import com.pensasha.emoney.user.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
