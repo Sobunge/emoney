2420634868b20c72b0fa5fb5e6719df6c2c6701a
Samuel Obunge <samuelobunge@gmail.com>
2023-09-07 01:20:29 +0300

    optimize tenant controller

--- src/main/java/com/pensasha/emoney/tenant/TenantController.java
+++ src/main/java/com/pensasha/emoney/tenant/TenantController.java
@@ -1,22 +1,14 @@
 package com.pensasha.emoney.tenant;
 
 import java.security.Principal;
-import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.pensasha.emoney.enums.Role;
 import com.pensasha.emoney.user.UserService;
-
-import jakarta.validation.Valid;
 
 @Controller
 public class TenantController {
